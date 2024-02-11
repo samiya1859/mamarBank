4358e29b9b02fa72480c2668b3955e68b1927166
samiya1859 <samiya1859@gmail.com>
2024-02-11 12:13:24 +0600

    allowed host

--- mamarbank/settings.py
+++ mamarbank/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamarbank2.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
